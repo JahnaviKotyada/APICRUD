ab40c24c278bbf11e2c161d7fd3326c2d31b71ef
JahnaviKotyada <jkotyada@zetaglobal.com>
2024-06-11 15:49:36 +0530

    Update user.go

--- API/models/user.go
+++ API/models/user.go
@@ -1,24 +1,8 @@
 package models
 
-import (
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
 type User struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-var DB *gorm.DB
-
-func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=jahnavi@2003 dbname=info port=5432 sslmode=disable"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect database")
-	}
-	database.AutoMigrate(&User{})
-	DB = database
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Addresses []Address `json:"addresses" gorm:"foreignKey:UserID"`
 }
